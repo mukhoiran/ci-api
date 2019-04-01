0b605f331e8e9f1e0324cab0d64b07961cb738ea
mukhoiran <mkhoir.anam@gmail.com>
2019-04-01 16:27:59 +0700

    Config autoload, db, and routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+//======== ROUTE API =======================================
+$route['api/users']['GET'] = 'UserController/get_all';
+$route['api/user/(:num)']['GET'] = 'UserController/get/$1';
+$route['api/register']['POST'] = 'UserController/save';
+$route['api/user/(:num)']['PUT'] = 'UserController/update/$1';
+$route['api/user/(:num)']['DELETE'] = 'UserController/delete/$1';
+$route['api/login'] = 'UserController/login';
